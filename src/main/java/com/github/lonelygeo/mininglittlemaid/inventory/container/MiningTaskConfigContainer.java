3d7ff89485638e3ddfaead28ce51a3280291104d
CheukWang Leung <2964476948des@gmail.com>
2026-04-28 14:28:33 +1000

    fix: 统一MenuType实例，修复打开任务配置Tab时的ID crashed

--- src/main/java/com/github/lonelygeo/mininglittlemaid/inventory/container/MiningTaskConfigContainer.java
+++ src/main/java/com/github/lonelygeo/mininglittlemaid/inventory/container/MiningTaskConfigContainer.java
@@ -1,17 +1,13 @@
 package com.github.lonelygeo.mininglittlemaid.inventory.container;
 
+import com.github.lonelygeo.mininglittlemaid.MiningLittleMaid;
 import com.github.tartaricacid.touhoulittlemaid.inventory.container.task.TaskConfigContainer;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.world.entity.player.Inventory;
-import net.minecraft.world.inventory.MenuType;
-import net.neoforged.neoforge.common.extensions.IMenuTypeExtension;
 
 public class MiningTaskConfigContainer extends TaskConfigContainer {
-    public static final MenuType<MiningTaskConfigContainer> TYPE =
-            IMenuTypeExtension.create(MiningTaskConfigContainer::new);
-
     public MiningTaskConfigContainer(int containerId, Inventory inventory, int entityId) {
-        super(TYPE, containerId, inventory, entityId);
+        super(MiningLittleMaid.MINING_TASK_CONFIG.get(), containerId, inventory, entityId);
     }
 
     public MiningTaskConfigContainer(int containerId, Inventory inventory, FriendlyByteBuf buf) {
