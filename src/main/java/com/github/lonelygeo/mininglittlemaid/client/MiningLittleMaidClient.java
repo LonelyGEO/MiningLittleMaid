510f27ce1dc1464bc2bc2fef95feea3882a63966
CheukWang Leung <2964476948des@gmail.com>
2026-04-28 14:28:33 +1000

    fix: 统一MenuType实例，修复打开任务配置Tab时的ID crashed

--- src/main/java/com/github/lonelygeo/mininglittlemaid/client/MiningLittleMaidClient.java
+++ src/main/java/com/github/lonelygeo/mininglittlemaid/client/MiningLittleMaidClient.java
@@ -2,11 +2,9 @@ package com.github.lonelygeo.mininglittlemaid.client;
 
 import com.github.lonelygeo.mininglittlemaid.MiningLittleMaid;
 import com.github.lonelygeo.mininglittlemaid.client.gui.MiningTaskConfigGui;
-import com.github.lonelygeo.mininglittlemaid.inventory.container.MiningTaskConfigContainer;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
-import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.neoforge.client.event.RegisterMenuScreensEvent;
 
 @EventBusSubscriber(modid = MiningLittleMaid.MOD_ID, value = Dist.CLIENT, bus = EventBusSubscriber.Bus.MOD)
@@ -14,6 +12,6 @@ public class MiningLittleMaidClient {
 
     @SubscribeEvent
     public static void registerScreens(RegisterMenuScreensEvent event) {
-        event.register(MiningTaskConfigContainer.TYPE, MiningTaskConfigGui::new);
+        event.register(MiningLittleMaid.MINING_TASK_CONFIG.get(), MiningTaskConfigGui::new);
     }
 }
